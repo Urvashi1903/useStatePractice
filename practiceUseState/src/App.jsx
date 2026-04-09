10293792ff19dbd63d8013a4b8624552d712be13
Urvashi Prajapat <urvashiumaprajapat@gmail.com>
2026-04-09 14:37:35 +0530

    Completed

--- practiceUseState/src/App.jsx
+++ practiceUseState/src/App.jsx
@@ -3,19 +3,21 @@ import React, { useState } from 'react'
 const App = () => {
   const [num, setNum] = useState(0);
 
-  function increaseBy1(){
-    setNum(num+1);
+  function increaseBy1() {
+    setNum(num + 1);
   }
 
-  function decreaseBy1(){
-    setNum(num-1);
+  function decreaseBy1() {
+    setNum(num - 1);
   }
 
   return (
     <div>
       <h1>{num}</h1>
-      <button onClick={increaseBy1}>Increase</button>
-      <button onClick={decreaseBy1}>Decrease</button>
+      <div className='btn'>
+        <button onClick={decreaseBy1}>Decrease by 1</button>
+        <button onClick={increaseBy1}>Increase by 1</button>
+      </div>
     </div>
   )
 }
